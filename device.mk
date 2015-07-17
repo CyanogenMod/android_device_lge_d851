a8441c3f9ad8a2bcd207c882431c80f3685e62d8
Eliminater74 <eliminater74@gmail.com>
2015-07-16 20:56:04 -0400

    Added my old Config files for new updated device tree

--- device.mk
+++ device.mk
@@ -31,6 +31,20 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf
 
+# Thermal
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine-8974.conf:system/etc/thermal-engine-8974.conf
+
+# Select Tmobile LTE IPV6 on first boot
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/apn/apns-conf.xml:system/etc/apns-conf.xml
+
+# Personal Packages
+PRODUCT_PACKAGES += \
+    OmniSwitch \
+    OmniGears \
+    OTAUpdates \
+    FloatingActionButton
 # Wifi
 PRODUCT_PACKAGES += \
     libnetcmdiface
@@ -39,6 +53,26 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     NfcNci \
     nfc_nci.pn54x.default
+    
+ #Viper4Android
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/viper4android/ViPER4Android_FX_A4.x.apk:system/priv-app/Viper4Android/viper4android.apk \
+    $(LOCAL_PATH)/viper4android/lib/libv4a_fx_ics_NEON_HQ.so:system/lib/soundfx/libv4a_fx_ics_NEON_HQ.so \
+    $(LOCAL_PATH)/viper4android/conf/audio_effects.conf:system/etc/audio_effects.conf
+    
+#OTA Updater
+#PRODUCT_COPY_FILES += \
+#    $(LOCAL_PATH)/OTAUpdates/lib/armeabi-v7a/libbypass.so:system/app/OTAUpdates/lib/arm/libbypass.so \
+#    $(LOCAL_PATH)/OTAUpdates/OTAUpdates.apk:system/app/OTAUpdates/base.apk
+  
+#Quick Remote Workaround
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/QuickRemote/addon.d/80-quickremote.sh:system/addon.d/80-quickremote.sh \
+    $(LOCAL_PATH)/QuickRemote/app/LGQRemote/LGQRemote.apk:system/app/LGQRemote/LGQRemote.apk \
+    $(LOCAL_PATH)/QuickRemote/app/QuicksetSDK/QuicksetSDK.apk:system/app/QuicksetSDK/QuicksetSDK.apk \
+    $(LOCAL_PATH)/QuickRemote/etc/init.d/10QuickRemote:system/etc/init.d/10QuickRemote \
+    $(LOCAL_PATH)/QuickRemote/media/audio/ui/IR_normal.ogg:system/media/audio/ui/IR_normal.ogg
+   
 
 # Get BCMDHD configs
 $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk)
